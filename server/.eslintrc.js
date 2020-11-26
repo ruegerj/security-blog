9d113d393a9120ea7dbf2d3b1717789342579166
ruegerj <jan.rueger@gmx.ch>
2020-11-26 22:45:13 +0100

    added custom eslint rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -9,5 +9,19 @@ module.exports = {
 		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
 		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
 	],
-	rules: {},
+	rules: {
+		'prettier/prettier': 'error',
+		'spaced-comment': 'off',
+		'no-console': 'warn',
+		'consistent-return': 'off',
+		'func-names': 'off',
+		'object-shorthand': 'off',
+		'no-process-exit': 'off',
+		'no-param-reassign': 'off',
+		'no-return-await': 'off',
+		'no-underscore-dangle': 'off',
+		'class-methods-use-this': 'off',
+		'prefer-destructuring': ['error', { object: true, array: false }],
+		'no-unused-vars': ['error', { argsIgnorePattern: 'req|res|next|val' }],
+	},
 };
