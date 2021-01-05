5c57a619c05333cb8eed290605314285de12c2f2
ruegerj <jan.rueger@gmx.ch>
2021-01-05 15:16:15 +0100

    added md bootstrap

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
 	declarations: [],
-	imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
-	exports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule],
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		RouterModule,
+		MDBBootstrapModule.forRoot(),
+	],
+	exports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		RouterModule,
+		MDBBootstrapModule,
+	],
 })
 export class SharedModule {}
