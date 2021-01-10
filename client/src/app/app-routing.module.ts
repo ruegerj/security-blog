783d27d3344f2c90256fc15912ef193991f8b5fd
ruegerj <jan.rueger@gmx.ch>
2021-01-10 16:40:43 +0100

    fixed global redirect issue

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,37 +6,23 @@ import { ContentLayoutComponent } from './layout/content-layout/content-layout.c
 
 const routes: Routes = [
 	{
+		path: 'user',
+		component: ContentLayoutComponent,
+		loadChildren: () =>
+			import('@modules/user/user.module').then((m) => m.UserModule),
+	},
+	{
+		path: 'home',
+		component: ContentLayoutComponent,
+		loadChildren: () =>
+			import('@modules/home/home.module').then((m) => m.HomeModule),
+	},
+	{
 		path: '',
 		component: AuthLayoutComponent,
 		canActivateChild: [NoAuthGuard],
 		loadChildren: () =>
 			import('@modules/auth/auth.module').then((m) => m.AuthModule),
-	},
-	{
-		path: '',
-		component: ContentLayoutComponent,
-		children: [
-			{
-				path: 'user',
-				loadChildren: () =>
-					import('@modules/user/user.module').then(
-						(m) => m.UserModule,
-					),
-			},
-			{
-				path: 'home',
-				loadChildren: () =>
-					import('@modules/home/home.module').then(
-						(m) => m.HomeModule,
-					),
-			},
-			// Fallback for unmatched routes
-			{
-				path: '**',
-				redirectTo: '/home',
-				pathMatch: 'full',
-			},
-		],
 	},
 ];
 
